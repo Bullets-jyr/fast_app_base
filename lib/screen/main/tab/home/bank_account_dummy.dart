c422b238cf1600108a96244c1da732beb95cbef7
Bullets-jyr <jyr@bullets.co.kr>
2023-10-05 12:57:37 +0900

    07. 기초, List, Map 데이터 구조

--- lib/screen/main/tab/home/bank_account_dummy.dart
+++ lib/screen/main/tab/home/bank_account_dummy.dart
@@ -1,3 +1,4 @@
+import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/screen/main/tab/home/banks_dummny.dart';
 import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
 
@@ -8,12 +9,41 @@ final bankAccountToss = BankAccount(bankTtoss, 5000000);
 final bankAccountKakao = BankAccount(bankKakao, 7000000, accountTypeName: '입출금통장');
 
 main() {
-  print(backAccounts[0].accountTypeName);
+  // print(backAccounts[0].accountTypeName);
+  //
+  // for(final item in backAccounts) {
+  //   print(item.accountTypeName);
+  // }
+
+  final shinhanBank = bankMap['shinhan1'];
+  // print(shinhanBank == bankAccountShinhan1);
+
+  // for(final entry in bankMap.entries) {
+  //   print(entry.key + ":" + (entry.value.accountTypeName ?? entry.value.bank.name));
+  // }
+
+  // Set
+  print(bankSet.contains(bankAccountShinhan1));
+
+  // List
+  print(bankAccounts.contains(bankAccountShinhan1));
+
+  // List와 Set에서 contains의 차이점은 시간복잡도에 있습니다.
+  // List에서 contains함수를 쓰게되면 이 List를 모두 한바퀴 돌면서 bankAccountShinhan1있는지 확인을 하게 됩니다. O(n)
+  // Set에서 contains를 하게되면 Set안에 있는 Hash Table을 통해서 즉시 있는지 없는지를 판단하게됩니다. O(1)
+  // 시간복잡도가 상수라는 것은 아무리 이 Map안에 몇만개, 몇백만개의 데이터가 들어있더라도 바로 즉시 있는지 없는지 여부를 알아낼 수 있다는 뜻입니다.
+
+  // List to Set
+  // 성능이 필요할 때는 Set
+  bankAccounts.toSet();
+
+  // Set to List
+  // 순회가 필요할 때는 List
+  bankSet.toList();
 }
 
-
-// List
-final List<BankAccount> backAccounts = [
+// List (중복허용)
+final List<BankAccount> bankAccounts = [
   bankAccountShinhan1,
   bankAccountShinhan2,
   bankAccountShinhan3,
@@ -21,6 +51,17 @@ final List<BankAccount> backAccounts = [
   bankAccountKakao,
 ];
 
-// Map
+// Map (중복불가)
+final bankMap = {
+  'shinhan1': bankAccountShinhan1,
+  'shinhan2': bankAccountShinhan2,
+};
 
-// Set
+// Set: (중복불가) 데이터를 가져오기 위해서 보통 사용하지 않고, 데이터가 존재하는지 체크하는 용도로 많이 사용합니다.
+final bankSet = {
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao,
+};
